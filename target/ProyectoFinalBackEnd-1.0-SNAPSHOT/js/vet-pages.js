1892f40b22868908f263896963572bd725d4ddb2
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-12-04 16:44:49 -0500

    Se areglan errores en las paginas

---
+++ target/ProyectoFinalBackEnd-1.0-SNAPSHOT/js/vet-pages.js
@@ -0,0 +1,13 @@
+document.querySelector("#list-visits").addEventListener("click", changePage1);
+document.querySelector("#add-visits").addEventListener("click", changePage2);
+document.querySelector("#update-visits").addEventListener("click", changePage3);
+
+function changePage1() {
+    location.href ='http://localhost:8080/ProyectoFinalBackEnd-1.0-SNAPSHOT/VetAddVisit.html';
+}
+function changePage2() {
+    location.href ='http://localhost:8080/ProyectoFinalBackEnd-1.0-SNAPSHOT/VetListVisits.html';
+}
+function changePage3() {
+    location.href ='http://localhost:8080/ProyectoFinalBackEnd-1.0-SNAPSHOT/VetUpdate.html';
+}
