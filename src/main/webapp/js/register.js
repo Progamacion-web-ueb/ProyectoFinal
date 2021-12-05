2ca7e4376d01c6ddf60b26d89baceb4924583ee2
AndresLeonardoCuadros <acuadrosb@unbosque.edu.co>
2021-12-04 19:48:00 -0500

    correcion paguinas

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -2,6 +2,14 @@ document.querySelector("#register").addEventListener("click", changePage);
 
 function changePage() {
 
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+    var neighborhood = document.getElementById("neighborhood").value;
+    var role = document.getElementById("role").value;
+
     location.href ='http://localhost:8080/ProyectoFinalBackEnd-1.0-SNAPSHOT/PublicRecord.html';
 }
 
--- target/ProyectoFinalBackEnd-1.0-SNAPSHOT/js/register.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#register").addEventListener("click", changePage);
-
-function changePage() {
-
-    location.href ='http://localhost:8080/ProyectoFinalBackEnd-1.0-SNAPSHOT/PublicRecord.html';
-}
-
-
