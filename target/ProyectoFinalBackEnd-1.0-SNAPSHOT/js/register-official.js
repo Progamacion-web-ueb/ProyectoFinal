995416f9b7e51fa529fc2f152dc4dd1c46b225ba
AndresLeonardoCuadros <acuadrosb@unbosque.edu.co>
2021-12-05 12:15:13 -0500

    Cambios css

--- target/ProyectoFinalBackEnd-1.0-SNAPSHOT/js/register-official.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#register-official").addEventListener("click", changePage);
-
-function changePage() {
-
-    location.href ='http://localhost:8080/ProyectoFinalBackEnd-1.0-SNAPSHOT/PrivateRecord.html';
-}
